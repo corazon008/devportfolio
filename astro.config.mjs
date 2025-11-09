81714e0a7e1a393b29480288d68ebbde89f06a75
corazon <99136079+corazon008@users.noreply.github.com>
2025-11-09 16:38:08 +0100

    Add github actions to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://corazon008.github.io",
+  base: "Portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
