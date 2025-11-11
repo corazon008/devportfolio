762bca088800a93a8b8cfe9c7fd21332598bfc59
corazon <99136079+corazon008@users.noreply.github.com>
2025-11-11 14:25:17 +0100

    Feat: Add personal data in Franch
    Languages are hardcoded and can not change dynamically

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,84 @@
-export const siteConfig = {
+export const siteConfig = siteConfigFr;
+
+export const siteConfigFr = {
+    name: "Corentin CALVEZ",
+    title: "Etudiant en école d'ingénieur",
+    description: "Site portfolio de Corentin CALVEZ",
+    accentColor: "#1d4ed8",
+    social: {
+        email: "corentin@bronevez.uk",
+        linkedin: "https://linkedin.com/in/corentin-calvez",
+        github: "https://github.com/corazon008",
+    },
+    aboutMe:
+        "Étudiant en intelligence artificielle engagé et passionné, avec une solide formation en programmation et développement logiciel. Désireux d'appliquer mes compétences dans des projets réels et de contribuer à des solutions innovantes.",
+    skills: ["Python", "C++", "Java", "Docker", "Linux", "Git"],
+    projects: [
+        {
+            name: "API Alcuin Calendar",
+            description:
+                "RESTful API pour accéder aux données du calendrier d'Alcuin, développée avec Python, Flask et Selenium",
+            link: "https://github.com/corazon008/AlcuinCalendar",
+            skills: ["Python", "Flask", "Selenium", "Docker"],
+        },
+        {
+            name: "Unofficial StartMining App",
+            description:
+                "Application Android pour surveiller vos récompenses de la plateforme StartMining, développée avec Java et Android SDK",
+            link: "https://github.com/corazon008/Unofficial-StartminingApp",
+            skills: ["Kotlin", "Java", "Blockchain"],
+        },
+        {
+            name: "RealTime Classifier",
+            description:
+                "Classificateur vidéo en temps réel utilisant YOLO et OpenCV, développé avec Python",
+            link: "",
+            skills: ["Python", "OpenCV", "YOLO", "Docker"],
+        },
+    ],
+    experience: [
+        {
+            company: "University of West Attica",
+            title: "Stage à l'University of West Attica",
+            dateRange: "Jun - Jul 2025",
+            bullets: [
+                "Développement d'une application Android pour la communication avec des dispositifs Bluetooth",
+                "Apprentissage des systèmes embarqués et des applications IoT",
+            ],
+        },
+        {
+            company: "SDIS49",
+            title: "Stage au SDIS49 dans le département informatique",
+            dateRange: "Feb 2022",
+            bullets: [
+                "Mise à niveau de la version PHP du serveur web et résolution des changements incompatibles",
+                "Apprentissage des meilleures pratiques de sécurité et de la gestion des serveurs (ISO 27001)",
+            ],
+        },
+        {
+            company: "IGRECA",
+            title: "Ouvrier en emballage de poudre",
+            dateRange: "Feb 2025",
+            bullets: [
+                "Remplir les boîtes de poudre et les emballer",
+                "Déplacer les boîtes sur une palette",
+            ],
+        },
+    ],
+    education: [
+        {
+            school: "ESAIP Angers",
+            degree: "Diplôme d'ingénieur en informatique et intelligence artificielle",
+            dateRange: "2022 - 2027",
+            achievements: [
+                "Semestre ERASMUS en Grèce, Espagne et République Tchèque",
+                "Cours de la Cisco Networking Academy complétés",
+            ],
+        },
+    ],
+};
+
+export const siteConfigEn = {
     name: "Corentin CALVEZ",
     title: "Student in Engineering School",
     description: "Portfolio website of Corentin CALVEZ",
@@ -9,13 +89,13 @@ export const siteConfig = {
         github: "https://github.com/corazon008",
     },
     aboutMe:
-        "Commited and passionate computer science student with a strong foundation in programming and software development. Eager to apply my skills in real-world projects and contribute to innovative solutions.",
+        "Commited and passionate student in artificial intelligence with a strong foundation in programming and software development. Eager to apply my skills in real-world projects and contribute to innovative solutions.",
     skills: ["Python", "C++", "Java", "Docker", "Linux", "Git"],
     projects: [
         {
             name: "API Alcuin Calendar",
             description:
-                "RESTful API for accessing Alcuin School calendar data, built with Python, Flask and Selenium",
+                "RESTful API for accessing Alcuin calendar data, built with Python, Flask and Selenium",
             link: "https://github.com/corazon008/AlcuinCalendar",
             skills: ["Python", "Flask", "Selenium", "Docker"],
         },
@@ -30,7 +110,7 @@ export const siteConfig = {
             name: "RealTime Classifier",
             description:
                 "Real-time video classifier using YOLO and OpenCV, built with Python",
-            link: "https://extensionkit.io/?ref=devportfolio",
+            link: "",
             skills: ["Python", "OpenCV", "YOLO", "Docker"],
         },
     ],
