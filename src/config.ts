22397b1daa32e9c0a76dc6b4c13eb61fe6f49b67
corazon <99136079+corazon008@users.noreply.github.com>
2026-02-09 17:19:37 +0100

    chore: reformat code

--- src/config.ts
+++ src/config.ts
@@ -1,159 +1,158 @@
 export const siteConfigFr = {
-    name: "Corentin CALVEZ",
-    title: "Etudiant en école d'ingénieur",
-    description: "Site portfolio de Corentin CALVEZ",
-    accentColor: "#1d4ed8",
-    social: {
-        email: "corentin@bronevez.uk",
-        linkedin: "https://linkedin.com/in/corentin-calvez",
-        github: "https://github.com/corazon008",
+  name: "Corentin CALVEZ",
+  title: "Etudiant en école d'ingénieur",
+  description: "Site portfolio de Corentin CALVEZ",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "corentin@bronevez.uk",
+    linkedin: "https://linkedin.com/in/corentin-calvez",
+    github: "https://github.com/corazon008",
+  },
+  aboutMe:
+    "Étudiant en intelligence artificielle engagé et passionné, avec une solide formation en programmation et développement logiciel. Désireux d'appliquer mes compétences dans des projets réels et de contribuer à des solutions innovantes.",
+  skills: ["Python", "C++", "Java", "Docker", "Linux", "Git"],
+  projects: [
+    {
+      name: "API Alcuin Calendar",
+      description:
+        "RESTful API pour accéder aux données du calendrier d'Alcuin, développée avec Python, Flask et Selenium",
+      link: "https://github.com/corazon008/AlcuinCalendar",
+      skills: ["Python", "Flask", "Selenium", "Docker"],
     },
-    aboutMe:
-        "Étudiant en intelligence artificielle engagé et passionné, avec une solide formation en programmation et développement logiciel. Désireux d'appliquer mes compétences dans des projets réels et de contribuer à des solutions innovantes.",
-    skills: ["Python", "C++", "Java", "Docker", "Linux", "Git"],
-    projects: [
-        {
-            name: "API Alcuin Calendar",
-            description:
-                "RESTful API pour accéder aux données du calendrier d'Alcuin, développée avec Python, Flask et Selenium",
-            link: "https://github.com/corazon008/AlcuinCalendar",
-            skills: ["Python", "Flask", "Selenium", "Docker"],
-        },
-        {
-            name: "Unofficial StartMining App",
-            description:
-                "Application Android pour surveiller vos récompenses de la plateforme StartMining, développée avec Java et Android SDK",
-            link: "https://github.com/corazon008/Unofficial-StartminingApp",
-            skills: ["Kotlin", "Java", "Blockchain"],
-        },
-        {
-            name: "RealTime Classifier",
-            description:
-                "Classificateur vidéo en temps réel utilisant YOLO et OpenCV, développé avec Python",
-            link: "",
-            skills: ["Python", "OpenCV", "YOLO", "Docker"],
-        },
-    ],
-    experience: [
-        {
-            company: "University of West Attica",
-            title: "Stage à l'University of West Attica",
-            dateRange: "Jun - Jul 2025",
-            bullets: [
-                "Développement d'une application Android pour la communication avec des dispositifs Bluetooth",
-                "Apprentissage des systèmes embarqués et des applications IoT",
-            ],
-        },
-        {
-            company: "SDIS49",
-            title: "Stage au SDIS49 dans le département informatique",
-            dateRange: "Feb 2022",
-            bullets: [
-                "Mise à niveau de la version PHP du serveur web et résolution des changements incompatibles",
-                "Apprentissage des meilleures pratiques de sécurité et de la gestion des serveurs (ISO 27001)",
-            ],
-        },
-        {
-            company: "IGRECA",
-            title: "Ouvrier en emballage de poudre",
-            dateRange: "Feb 2025",
-            bullets: [
-                "Remplir les boîtes de poudre et les emballer",
-                "Déplacer les boîtes sur une palette",
-            ],
-        },
-    ],
-    education: [
-        {
-            school: "ESAIP Angers",
-            degree: "Diplôme d'ingénieur en informatique et intelligence artificielle",
-            dateRange: "2022 - 2027",
-            achievements: [
-                "Semestre ERASMUS en Grèce, Espagne et République Tchèque",
-                "Score TOEIC : 910/990",
-                "Cours de la Cisco Networking Academy complétés",
-            ],
-        },
-    ],
+    {
+      name: "Unofficial StartMining App",
+      description:
+        "Application Android pour surveiller vos récompenses de la plateforme StartMining, développée avec Java et Android SDK",
+      link: "https://github.com/corazon008/Unofficial-StartminingApp",
+      skills: ["Kotlin", "Java", "Blockchain"],
+    },
+    {
+      name: "RealTime Classifier",
+      description:
+        "Classificateur vidéo en temps réel utilisant YOLO et OpenCV, développé avec Python",
+      link: "",
+      skills: ["Python", "OpenCV", "YOLO", "Docker"],
+    },
+  ],
+  experience: [
+    {
+      company: "University of West Attica",
+      title: "Stage à l'University of West Attica",
+      dateRange: "Jun - Jul 2025",
+      bullets: [
+        "Développement d'une application Android pour la communication avec des dispositifs Bluetooth",
+        "Apprentissage des systèmes embarqués et des applications IoT",
+      ],
+    },
+    {
+      company: "SDIS49",
+      title: "Stage au SDIS49 dans le département informatique",
+      dateRange: "Feb 2022",
+      bullets: [
+        "Mise à niveau de la version PHP du serveur web et résolution des changements incompatibles",
+        "Apprentissage des meilleures pratiques de sécurité et de la gestion des serveurs (ISO 27001)",
+      ],
+    },
+    {
+      company: "IGRECA",
+      title: "Ouvrier en emballage de poudre",
+      dateRange: "Feb 2025",
+      bullets: [
+        "Remplir les boîtes de poudre et les emballer",
+        "Déplacer les boîtes sur une palette",
+      ],
+    },
+  ],
+  education: [
+    {
+      school: "ESAIP Angers",
+      degree:
+        "Diplôme d'ingénieur en informatique et intelligence artificielle",
+      dateRange: "2022 - 2027",
+      achievements: [
+        "Semestre ERASMUS en Grèce, Espagne et République Tchèque",
+        "Score TOEIC : 910/990",
+        "Cours de la Cisco Networking Academy complétés",
+      ],
+    },
+  ],
 };
 
 export const siteConfigEn = {
-    name: "Corentin CALVEZ",
-    title: "Student in Engineering School",
-    description: "Portfolio website of Corentin CALVEZ",
-    accentColor: "#1d4ed8",
-    social: {
-        email: "corentin@bronevez.uk",
-        linkedin: "https://linkedin.com/in/corentin-calvez",
-        github: "https://github.com/corazon008",
+  name: "Corentin CALVEZ",
+  title: "Student in Engineering School",
+  description: "Portfolio website of Corentin CALVEZ",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "corentin@bronevez.uk",
+    linkedin: "https://linkedin.com/in/corentin-calvez",
+    github: "https://github.com/corazon008",
+  },
+  aboutMe:
+    "Commited and passionate student in artificial intelligence with a strong foundation in programming and software development. Eager to apply my skills in real-world projects and contribute to innovative solutions.",
+  skills: ["Python", "C++", "Java", "Docker", "Linux", "Git"],
+  projects: [
+    {
+      name: "API Alcuin Calendar",
+      description:
+        "RESTful API for accessing Alcuin calendar data, built with Python, Flask and Selenium",
+      link: "https://github.com/corazon008/AlcuinCalendar",
+      skills: ["Python", "Flask", "Selenium", "Docker"],
     },
-    aboutMe:
-        "Commited and passionate student in artificial intelligence with a strong foundation in programming and software development. Eager to apply my skills in real-world projects and contribute to innovative solutions.",
-    skills: ["Python", "C++", "Java", "Docker", "Linux", "Git"],
-    projects: [
-        {
-            name: "API Alcuin Calendar",
-            description:
-                "RESTful API for accessing Alcuin calendar data, built with Python, Flask and Selenium",
-            link: "https://github.com/corazon008/AlcuinCalendar",
-            skills: ["Python", "Flask", "Selenium", "Docker"],
-        },
-        {
-            name: "Unofficial StartMining App",
-            description:
-                "Android app to monitor your rewards from the StartMining platform, built with Java and Android SDK",
-            link: "https://github.com/corazon008/Unofficial-StartminingApp",
-            skills: ["Kotlin", "Java", "Blockchain"],
-        },
-        {
-            name: "RealTime Classifier",
-            description:
-                "Real-time video classifier using YOLO and OpenCV, built with Python",
-            link: "",
-            skills: ["Python", "OpenCV", "YOLO", "Docker"],
-        },
-    ],
-    experience: [
-        {
-            company: "University of West Attica",
-            title: "Internship in University of West Attica",
-            dateRange: "Jun - Jul 2025",
-            bullets: [
-                "Android application development for commination with Bluetooth devices",
-                "Learned about embedded systems and IoT applications",
-            ],
-        },
-        {
-            company: "SDIS49",
-            title: "Internship in SDIS49 in IT department",
-            dateRange: "Feb 2022",
-            bullets: [
-                "Upgrade PHP version of web server and solve breaking changes",
-                "Learned about security best practices and server management (ISO 27001)",
-            ],
-        },
-        {
-            company: "IGRECA",
-            title: "Powder Packaging Worker",
-            dateRange: "Feb 2025",
-            bullets: [
-                "Fill powder boxes and pack them",
-                "Move boxes onto a pallet",
-            ],
-        },
-    ],
-    education: [
-        {
-            school: "ESAIP Angers",
-            degree: "Engineering Degree in Computer Science and Artificial Intelligence",
-            dateRange: "2022 - 2027",
-            achievements: [
-                "ERASMUS semester in Greece, Spain and Czech Republic",
-                "TOEIC Score: 910/990",
-                "Cisco Networking Academy courses completed",
-            ],
-        },
-    ],
+    {
+      name: "Unofficial StartMining App",
+      description:
+        "Android app to monitor your rewards from the StartMining platform, built with Java and Android SDK",
+      link: "https://github.com/corazon008/Unofficial-StartminingApp",
+      skills: ["Kotlin", "Java", "Blockchain"],
+    },
+    {
+      name: "RealTime Classifier",
+      description:
+        "Real-time video classifier using YOLO and OpenCV, built with Python",
+      link: "",
+      skills: ["Python", "OpenCV", "YOLO", "Docker"],
+    },
+  ],
+  experience: [
+    {
+      company: "University of West Attica",
+      title: "Internship in University of West Attica",
+      dateRange: "Jun - Jul 2025",
+      bullets: [
+        "Android application development for commination with Bluetooth devices",
+        "Learned about embedded systems and IoT applications",
+      ],
+    },
+    {
+      company: "SDIS49",
+      title: "Internship in SDIS49 in IT department",
+      dateRange: "Feb 2022",
+      bullets: [
+        "Upgrade PHP version of web server and solve breaking changes",
+        "Learned about security best practices and server management (ISO 27001)",
+      ],
+    },
+    {
+      company: "IGRECA",
+      title: "Powder Packaging Worker",
+      dateRange: "Feb 2025",
+      bullets: ["Fill powder boxes and pack them", "Move boxes onto a pallet"],
+    },
+  ],
+  education: [
+    {
+      school: "ESAIP Angers",
+      degree:
+        "Engineering Degree in Computer Science and Artificial Intelligence",
+      dateRange: "2022 - 2027",
+      achievements: [
+        "ERASMUS semester in Greece, Spain and Czech Republic",
+        "TOEIC Score: 910/990",
+        "Cisco Networking Academy courses completed",
+      ],
+    },
+  ],
 };
 
 export const siteConfig = siteConfigEn;
